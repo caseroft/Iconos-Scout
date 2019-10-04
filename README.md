9efdcd815107c0924a1d6292aa1a8917269c60d9
Andres Hal <caseroft@gmail.com>
2019-10-04 19:27:21 -0300

    Realizo una actualizacion completa de las librerias. restructuro los iconos (todavia en Borrador) renombro todo a OpenScouts

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Iconos-Scout
+
 Set de Iconos Scouts
 
+https://openscouts.github.io/Iconos-Scout/
 
-![Alt text](full.png?raw=true "Title")
+![Alt text](full.png?raw=true 'Title')
+
+## Licencias Creative Commons
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />
+
+### Bajo los siguientes términos:
+
+- **Atribución** — Usted debe dar [crédito de manera adecuada](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es#), brindar un enlace a la licencia, e [indicar si se han realizado cambios](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es#). Puede hacerlo en cualquier forma razonable, pero no de forma tal que sugiera que usted o su uso tienen el apoyo de la licenciante.
+
+- **NoComercial** — Usted no puede hacer uso del material con [propósitos comerciales](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es#).
+
+- **CompartirIgual** — Si remezcla, transforma o crea a partir del material, debe distribuir su contribución bajo la la[misma licencia](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es#) del original.
+
+Esta obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Licencia Creative Commons Atribución-NoComercial-CompartirIgual 4.0 Internacional</a>.
+
+- A acepción de las fotos de individuos que solo fue autorizado para utilizacion de esta pagina
+
+## Codigo Fuente
+
+[https://gitlab.com/openscouts/Iconos-Scout](https://gitlab.com/openscouts/Iconos-Scout)
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,42 @@
 var gulp = require('gulp'),
-    consolidate = require('gulp-consolidate'),
-    iconfont = require('gulp-iconfont');
+  consolidate = require('gulp-consolidate'),
+  iconfont = require('gulp-iconfont');
 
-gulp.task('default', function () {
-   return gulp.src('src/svg/*.svg')
-        .pipe(iconfont({
-            fontName: 'caseroft-iconos-scout',
-            formats: ['ttf', 'eot', 'woff', 'woff2'],
-            appendCodepoints: true,
-            appendUnicode: false,
-            normalize: true,
-            fontHeight: 1000,
-            centerHorizontally: true
-        }))
-        .on('glyphs', function (glyphs, options) {
-            gulp.src('src/caseroft-iconos-scout.css')
-                .pipe(consolidate('underscore', {
-                    glyphs: glyphs,
-                    fontName: options.fontName,
-                    fontDate: new Date().getTime()
-                }))
-                .pipe(gulp.dest('dist/css/'));
+gulp.task('default', function() {
+  return gulp
+    .src('src/svg/**/*.svg')
+    .pipe(
+      iconfont({
+        fontName: 'open-scouts',
+        formats: ['ttf', 'eot', 'woff', 'woff2'],
+        appendCodepoints: true,
+        appendUnicode: false,
+        normalize: true,
+        fontHeight: 1000,
+        centerHorizontally: true
+      })
+    )
+    .on('glyphs', function(glyphs, options) {
+      gulp
+        .src('src/open-scouts-iconos-fonts.css')
+        .pipe(
+          consolidate('underscore', {
+            glyphs: glyphs,
+            fontName: options.fontName,
+            fontDate: new Date().getTime()
+          })
+        )
+        .pipe(gulp.dest('dist/css/'));
 
-            gulp.src('src/index.html')
-                .pipe(consolidate('underscore', {
-                    glyphs: glyphs,
-                    fontName: options.fontName
-                }))
-                .pipe(gulp.dest('dist'));
-        })
-        .pipe(gulp.dest('dist/fonts'));
+      gulp
+        .src('src/index.html')
+        .pipe(
+          consolidate('underscore', {
+            glyphs: glyphs,
+            fontName: options.fontName
+          })
+        )
+        .pipe(gulp.dest('dist')); // el index lo muevo a doc, que es donde levanta la page de github
+    })
+    .pipe(gulp.dest('dist/fonts'));
 });
